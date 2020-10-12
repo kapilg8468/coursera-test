3b57680adb94e6e8c85ad583dc06e4b4b15cd62a
kapilg8468 <kapilg8468@gmail.com>
2020-10-13 03:46:32 +0530

    kapil

---
+++ site/script.js
@@ -0,0 +1,18 @@
+// function chk(a,b)
+// {
+// 	console.log("hi buddy ");
+// 	if(a>b)
+// 	console.log("a>b ");
+// else
+// 	console.log("a<b ");
+// }
+// x="Hi Kapil"
+// chk(4,5);
+// for(x=0;x<10;x++){
+// 	console.log("Buudy "+(x+1)+"\n");
+// }
+function chk(){
+var name ;
+name=" <h2>Hi buddy "+document.getElementById("name").value +  " jhakas </h1> ";
+document.getElementById("content").innerHTML=name;
+}
