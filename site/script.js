568eebbc497caecab774367ab037024f49981fc0
kapil <kapilg8468@gmail.com>
2020-11-18 14:06:36 +0530

    api 3

--- site/script.js
+++ site/script.js
@@ -1,16 +1,17 @@
 // function chk(a,b)
 // {
-// 	console.log("hi buddy ");
-// 	if(a>b)
-// 	console.log("a>b ");
+//     console.log("hi buddy ");
+//     if(a>b)
+//     console.log("a>b ");
 // else
-// 	console.log("a<b ");
+//     console.log("a<b ");
 // }
 // x="Hi Kapil"
 // chk(4,5);
 // for(x=0;x<10;x++){
-// 	console.log("Buudy "+(x+1)+"\n");
+//     console.log("Buudy "+(x+1)+"\n");
 // }
+/*jshint esversion: 6 */
 function chk(){
 var name ,greeting ,currentdate,hr,kp;
 var mon=["Jan","Feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"];
@@ -26,37 +27,34 @@ else
 greeting="Have a great night ";
 if(hr<12)
 kp="AM";
-else
-{kp="PM";hr-=12;}
-var datetime = ", Today is " + currentdate.getDate() + " " + mon[currentdate.getMonth()] 
-+ " " + currentdate.getFullYear() + "   " 
-+ hr + ":" 
-+ currentdate.getMinutes() + " "+ kp;
-
+else{
+    kp="PM";hr-=12;
+}
+var datetime = ", Today is " + currentdate.getDate() + " " + mon[currentdate.getMonth()] + " " + currentdate.getFullYear() + "   " + hr + ":" + currentdate.getMinutes() + " "+ kp;
 name=" <h1> "+greeting +document.getElementById("name").value +  " "+datetime +" </h1>";
 document.getElementById("content").innerHTML=name;
 }
 var mx=2000,cur=2000;
 function chk2(){
         const xhr = new XMLHttpRequest();
-        xhr.open('POST','https://codeforces.com/api/user.info?handles=buddy_1',false);
+        xhr.open('GET','https://codeforces.com/api/user.info?handles=buddy_1',false);
         // console.log('hi kapil');
         xhr.onload= function() {
             // console.log(this.responseText);
             let a1=JSON.parse(this.responseText);
-            for (key in a1){
-                if(key=== "result"){
+            var k,key;
+            for (k in a1){
+                if(k=== "result"){
                     //  mx=a1[key][0].maxRating;
                     // console.log(mx);
-                    let o2=a1[key];
-                    for(key in a1[key]){
+                    let o2=a1[k];
+                    for(key in a1[k]){
                     mx=o2[key].maxRating;
                     cur=o2[key].rating;     
                     }
                 }
             }
-            
-        }
+        };
         xhr.send();
        
 }
